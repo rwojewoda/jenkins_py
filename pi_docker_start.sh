a5980d2ff418fd40c30bb625d17bcbdc48b0ed13
Rado <radekspam5@gmail.com>
2019-03-12 18:41:24 +0100

    msg

--- pi_docker_start.sh
+++ pi_docker_start.sh
@@ -2,4 +2,4 @@
 
 docker pull davout123/py
 docker container rm -f dckr
-docker run -p 6601:6600 --user root --device /dev/snd --name dckr -d davout123/py
+docker run -p 6601:6601 --name dckr -d davout123/py
